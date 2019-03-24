1078515ff097580a95c23c28481f33def4ede7f2
Niffy <kttc.238@gmail.com>
2019-03-24 16:21:01 +0900

    体験会uploadテスト中

---
+++ GEEKJOB_TRIAL/src/camp/trial01/Level00.java
@@ -0,0 +1,17 @@
+package camp.trial01;
+
+/**
+ * @author nishi
+ *
+ */
+public class Level00 {
+	public static void main(String[] args) {
+		/*
+		 * 【課題00】
+		 * ファイルを実行し、「Hello, World!」と表示させよう。
+		 */
+		//ここから
+		System.out.println("Hello, World!");
+		//ここまで
+	}
+}
